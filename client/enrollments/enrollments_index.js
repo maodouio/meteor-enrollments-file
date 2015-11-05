508d01dbc61ee89d8668d7fc6bb7a84ee8030860
Jeff <609456713@qq.com>
2015-11-05 21:00:56 +0800

    remove redundant code

--- client/enrollments/enrollments_index.js
+++ client/enrollments/enrollments_index.js
@@ -3,9 +3,7 @@ Template.enrollmentsIndex.rendered = function() {
 
 Template.enrollmentsIndex.helpers({
   file: function () {
-    // console.log(this);
     var id = this.picture;
-    // console.log('id:', id);
     return Images.findOne({_id: id});
   },
 });
